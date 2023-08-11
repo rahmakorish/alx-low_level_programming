2a81408d464fe37d80a7d7c9cce0786439db8c5d
rahmakorish <rahmakorish@gmail.com>
2023-08-11 02:19:48 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/**
+ * main - this entry point for the program
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
