b74dc4ac109d74ad631b2d6fb9304f2d638a3bca
rahmakorish <rahmakorish@gmail.com>
2023-08-11 06:28:23 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ **main-entry
+ **Return:0succes
+ ***/
+int main(void)
+{
+	int i = 97;
+	int x = 65;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	};
+	while (x < 91)
+	{
+		putchar(x);
+		x++;
+	};
+	putchar(10);
+	return(0);
+}
