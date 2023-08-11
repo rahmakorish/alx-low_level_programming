a07e38618c3ae1ac557e880fc64926bcdb258812
rahmakorish <rahmakorish@gmail.com>
2023-08-11 03:49:26 -0700

    numbers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-*main-entry point 
+*main
 *Return: (0) mean program executed
 **/
 int main(void)
@@ -8,7 +8,7 @@ int main(void)
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long));
-	printf("Size of long long int: %ld byte(s)\n", sizeof(double));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(double));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
