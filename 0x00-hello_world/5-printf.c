4997d8dec4fe5b1b16a0023ba2a51821ce19fd2a
rahmakorish <rahmakorish@gmail.com>
2023-08-11 03:09:04 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  *main - this is entry point for program
  *prints a sentence 
  *Return: (0) if code executed without compilation error
- */
+ *********/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
