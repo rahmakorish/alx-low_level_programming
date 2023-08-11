264964218d2983f41c9370a650abbf6d9322c2c0
rahmakorish <rahmakorish@gmail.com>
2023-08-11 07:01:10 -0700

    alpha exception

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main-entry
+*Return:0succes
+**/
+int main(void)
+{	
+	int i = 97;
+
+	 while (i < 123)
+	{
+		if(i!=101)
+		if(i!=113)
+		
+		putchar(i);
+		i++;	
+	};
+	putchar(10);
+	return (0);
+}
