8082724a6e5b35814ad9c5dae09749fd506c1ef5
rahmakorish <rahmakorish@gmail.com>
2023-08-11 03:38:29 -0700

    numbers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- *main - entry point 
- *Return: 0 when succed
- **/
+*main - entry point 
+*Return: 0 when succed
+**/
 int main(void)
-{	
-	printf("size of a char: %ld byte(s)\n", sizeof(char));
-	printf("size of an int: %ld byte(S)\n", sizeof(int));
-	printf("size of a long int: %ld byte(S)\n", sizeof(long));
-	printf("size of long long int: %ld byte(s)\n", sizeof(double));
-	printf("size of a float: %ld byte(S)\n", sizeof(float));
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(S)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(S)\n", sizeof(long));
+	printf("Size of long long int: %ld byte(s)\n", sizeof(double));
+	printf("Size of a float: %ld byte(S)\n", sizeof(float));
+	return(0);
 }
