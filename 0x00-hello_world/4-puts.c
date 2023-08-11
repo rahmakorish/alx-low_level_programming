18477a55de50115d38b0f7b331985c30d6158b56
rahmakorish <rahmakorish@gmail.com>
2023-08-11 02:45:44 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - this entry point for the program
- * return: 0 if code executed 
- */
+ * return: 0 if code executed and a positive number otherwise
+ * */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
