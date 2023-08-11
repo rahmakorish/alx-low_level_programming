8a4cf441dc5ed23710ebc3b7e896710f605d009a
rahmakorish <rahmakorish@gmail.com>
2023-08-11 06:19:53 -0700

    alphabet:

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ **main-entry
+ **Return:0succes
+ ***/
+int main(void)
+{
+	int i = 97;
+
+	while(i < 122)
+	{
+		putchar(i);
+		i++;
+	};
+	putchar(10);
+	return (0);
+}
