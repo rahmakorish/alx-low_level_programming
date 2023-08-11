66742e6d38071b9fbf1003a8939adec1fb59c0c4
rahmakorish <rahmakorish@gmail.com>
2023-08-11 07:15:47 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+**main-entry
+**Return:0succes
+**/
+int main(void)
+{
+	int i = 48;
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	};
+	putchar(10);
+	return (0);
+}
