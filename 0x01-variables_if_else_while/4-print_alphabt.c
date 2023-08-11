3c16c0e288609ee895340ef08bb5674c453c35ce
rahmakorish <rahmakorish@gmail.com>
2023-08-11 07:06:06 -0700

    alpha exception

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,14 +6,13 @@
 *Return:0succes
 **/
 int main(void)
-{	
+{
 	int i = 97;
 
-	 while (i < 123)
+	while (i < 123)
 	{
-		if(i!=101)
-		if(i!=113)
-		
+		if (i != 101)
+		if (i != 113)
 		putchar(i);
 		i++;	
 	};
