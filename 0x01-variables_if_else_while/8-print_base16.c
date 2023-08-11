afdfbbc02ea6f9b431444ffea327b5ed9d5721e5
rahmakorish <rahmakorish@gmail.com>
2023-08-11 09:34:44 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <time.h>
+#include <stdio.h>
+/**
+ * **main-entry
+ * **Return:0succes
+ * **/
+int main(void)
+{
+	int i =  48;
+	int x = 97;
+
+	while (i > 58)
+	{
+		putchar(i);
+		i++;
+	}
+	while (x > 103)
+	{
+		putchar(x);
+		x++;
+	};
+	putchar(10);
+	return (0);
+}	
