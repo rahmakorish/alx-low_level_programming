67fb91e018477d9c9e14949c01a393e4c4abf45a
rahmakorish <rahmakorish@gmail.com>
2023-08-11 02:52:53 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is entry point for program
+ *
+ * return: (0) if code executed
+ */
+int main(void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
