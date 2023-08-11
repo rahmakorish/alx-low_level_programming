95e286523f8846790859bf7fe6517e8d11ba9b30
rahmakorish <rahmakorish@gmail.com>
2023-08-11 09:25:26 -0700

    reverse alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <time.h>
+#include <stdio.h>
+/**
+ * **main-entry
+ * **Return:0succes
+ * **/
+int main(void)
+{
+	int i = 122;
+
+	while(i>96)
+	{
+		putchar(i);
+		i--;
+	};
+	putchar(10);
+	return (0);
+}
