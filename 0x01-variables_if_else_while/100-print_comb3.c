70ee8528a5e51e2372305564db93c9e32e1f26be
rahmakorish <rahmakorish@gmail.com>
2023-08-11 11:53:21 -0700

    combo3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,15 +6,15 @@
  * **/
 int main(void)
 {
-	for(int i = 48; i<58;)
+	for(int i = 48; i<58; )
 	{ 
-
-		for(int x = 49; x<58; x++)
+		for(int x = 49;x < 58; x++)
 		{
 			putchar(i);
 			putchar(x);
 			putchar(44);
 			putchar(32);
+		
 		}
 		i++;
 	}
