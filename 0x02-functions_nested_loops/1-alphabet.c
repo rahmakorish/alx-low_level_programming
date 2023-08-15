ccacb94939acf74670d0b9ed5da04c93ebb27308
rahmakorish <rahmakorish@gmail.com>
2023-08-15 03:29:45 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,18 @@
 #include "main.h"
 
 /**
-*main-entry
-*Return:0succes
+*print_alphabet-prints alphabet in lower case
+*folowed by newline
+*Retun:0success
 **/
-
-	/**
-	  *print_alphabe-prints alphabet in lower case
-	  *folowed by newline
-	  *Retun:0success
-	  **/
 void print_alphabet(void)
 {
-	int i =97;
-	       
-	while(i < 123 )
+	int i = 97;
+
+	while (i < 123)
 	{
-		_putchar (i);
+		_putchar(i);
 		i++;
 	};
 	_putchar('\n');
-}	
+}
