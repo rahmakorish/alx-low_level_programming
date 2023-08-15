41b630ed66e2661c1d0ce876ec85c7c18074cfd8
rahmakorish <rahmakorish@gmail.com>
2023-08-15 10:15:18 -0700

    to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,12 +10,26 @@ void print_to_98(int n)
 {
 	while (n < 99)
 	{
-		printf("%d, ", n);
-		n++;
+		if (n == 98)
+		{
+			printf("%d", n);
+		}
+		else
+		{
+			printf("%d, ", n);
+			n++;
+		}
 	}
-	while (n > 99)
+	while (n > 97)
 	{
-		printf("%d, ", n);
-		n--;
+		if (n == 98)
+		{
+			printf("%d", n);
+		}
+		else
+		{
+			printf("%d, ", n);
+			n--;
+		}
 	}
 }
