025209d152193f046e9bd116f5f5a19b122b2245
rahmakorish <rahmakorish@gmail.com>
2023-08-15 12:55:15 -0700

    extra

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main-entry
+*Return:0succes
+**/
+int main(void)
+{
+	int x=1;
+	int y=2;
+	int count = 0;
+	
+	while (count < 51)
+	{
+		int z = x+y;
+		printf("%d, ", z);
+		y = x;
+		x = z;
+		count++;
+	}
+}
