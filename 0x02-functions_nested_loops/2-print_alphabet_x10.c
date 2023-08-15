220ca256aaef74c5a69973943193bf678e2350f6
rahmakorish <rahmakorish@gmail.com>
2023-08-15 03:38:25 -0700

    alpha 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10-prints alphabet ten times
+*Return:0succes
+**/
+void print_alphabet_x10(void)
+{
+	int x = 0;
+	int y = 97;
+
+	do
+	{
+		while (y < 123)
+		{
+			_putchar (y);
+		};
+	}
+	while (x < 9);
+}
