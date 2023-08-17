4ebe836b6da1ed419760a97d02d1b6283c7900da
rahmakorish <rahmakorish@gmail.com>
2023-08-17 05:56:03 -0700

    print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include "main.h"
 /**
 *print_numbers-prints the numbers from 0 to 9
 *Return:0succes
@@ -8,7 +9,7 @@
 void print_numbers(void)
 {
 	int i = 0;
-       
+
 	while (i < 1)
 	{
 		_putchar(i);
