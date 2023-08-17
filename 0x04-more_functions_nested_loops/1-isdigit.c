988164f0843997fa47658a1c17c7128891a1ed36
rahmakorish <rahmakorish@gmail.com>
2023-08-17 05:23:01 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*_isdigit-checks for a digit
+*@c:integer to be checked
+*Return:0succes
+**/
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	};
+}
