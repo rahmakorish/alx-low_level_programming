062184b89002f2e9b069bb8be9a8220c9c3877d6
rahmakorish <rahmakorish@gmail.com>
2023-08-21 10:30:59 -0700

    print

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts-prints a string followed by new line
+ * @str:strring to be printed
+ **/
+void _puts(char *str)
+{
+	while (*str++ != '\0')
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
