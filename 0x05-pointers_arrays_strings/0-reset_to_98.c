fd5e1500b59e75432d72f8c59c9e11aa3e19a18b
rahmakorish <rahmakorish@gmail.com>
2023-08-21 02:40:14 -0700

    first pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98-update the value in a pointerv vto 98
+ * @*n:pointer to an integer
+ * */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H 
 #include <stdio.h>
 
+
 int _putchar(char c);
+void reset_to_98(int *n);
 
 #endif
