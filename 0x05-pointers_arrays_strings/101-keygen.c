10d92640f54584aa40367d7a8b500e222821c13f
rahmakorish <rahmakorish@gmail.com>
2023-08-21 15:42:20 -0700

    random pass

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main-entry point
+ * @n:length of password required
+ * Return:0success
+ * */
+int main (void)
+{
+	int i, n = 20;
+	time_t t;
+
+	srand((unsigned) time(&t));
+	for (i = 0; i < n; i++)
+		printf("%c", rand());
+}
