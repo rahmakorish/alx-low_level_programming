2fb5b7a6c779e6306686fd03cdc6ce8c5542d86f
rahmakorish <rahmakorish@gmail.com>
2023-08-24 05:18:48 -0700

    1773

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,12 +9,25 @@ char *cap_string(char *s)
 {
 	int i;
 	int length = strlen(s);
-/*	char separators [] = {',', ';', '.', '!', '?', '"', '(', ')','{', '}'";*/
 
+int check(char *s)
+{
+int x,z;	
+char separators [] = {',', ';', '.', '!', '?','\n' ,'"', '(', ')','{',' ' ,'}'};
+for (x = 0; x < strlen(s); x++)
+for (z = 0; z < 11; z++)
+{
+	if (s[x] == separatores[z])
+	return (0);
+	else
+	return (1);
+}	
+
+checker = check(s);
 	
 	for (i = 0; i < length; i++)
 	{	
-		if (121 > s[i] && 96 < s[i] && s[i - 1] == ' ')
+		if (121 > s[i] && 96 < s[i] && checker) /*(s[i - 1] == ' ')*/
 		{
 			s[i] = s[i] - 32;
 		}
