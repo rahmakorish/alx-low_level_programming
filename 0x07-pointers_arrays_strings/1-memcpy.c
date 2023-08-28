9e907e878033018073f725cf18da974a0c14d270
rahmakorish <rahmakorish@gmail.com>
2023-08-28 06:11:56 -0700

    second task

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _memcpy - copies memory area
+ * @dest : memory set to paste
+ * @n : number of bytes 
+ * @src : memory area to copy from 
+ * Return : pointer to dest
+ **/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	dest[n] = src[n];
+	return (dest);
+}
