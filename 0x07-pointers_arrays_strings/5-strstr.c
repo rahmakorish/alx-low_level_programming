7e90480dbf888022543d45e00d5754834030fda4
rahmakorish <rahmakorish@gmail.com>
2023-08-28 14:51:16 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strstr- locates a substring
+ * @haystack:string to be searched
+ * @needle: substring to be located
+ * Return: pointer to the beginning of the substring
+ **/
+char *_strstr(char *haystack, char *needle)
+{
+}
