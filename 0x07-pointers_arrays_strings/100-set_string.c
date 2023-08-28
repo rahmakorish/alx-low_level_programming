cc453528071bbb29435e091e8ed505e7a5ae3d76
rahmakorish <rahmakorish@gmail.com>
2023-08-28 15:57:48 -0700

    extra

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ *set_string-setsthe value of a pointer to a char
+ *@s: pointer
+ *@to:char
+ **/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
