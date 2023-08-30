51e2c0d5f9a4e47e35e167a319a592bba2a0ced2
rahmakorish <rahmakorish@gmail.com>
2023-08-30 13:10:36 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,14 +3,29 @@
  * _sqrt_recursion-returns the square root of a number
  * @n: number to get square root
  * Return: square root of number n
+ */
+/*
+ *get_square- gets the square root of num
+ *@n: number to check
+ *@root:output num
+ *Return:root of n
  **/
+int get_square(int n, int root);
 int _sqrt_recursion(int n)
 {
-	int root = 1;
+	int rootfinal = get_square(n, 1);
 
+	return (rootfinal);
+}
+int get_square(int n, int root)
+{
 	if (root * root == n)
 	{
 		return (root);
+	}
+	else if (root * root < n)
+	{
+		return (get_square(n, root + 1));
 	}
 	else
 	{
