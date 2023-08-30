fb3655d1ee2cb56517107b0668e9e010b128496b
rahmakorish <rahmakorish@gmail.com>
2023-08-30 13:56:40 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,7 +4,41 @@
  * @n:number to check
  * Return: 1 if prime and o if otherwise
  **/
+int factorial(int n);
 int is_prime_number(int n)
 {
-	
+	int factor = factorial(n);
+
+	if (factor % n == n - 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+/**
+ * factorial-returns factorial of a given number
+ * @n:factorial number
+ * Return: factorial of given number
+ **/
+int factorial(int n)
+{
+	int fact = 0;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		fact += (n * factorial(n - 1));
+		/*factorial(n-1);*/
+	}
+	else if (n == 0)
+	{
+		fact = 1;
+	}
+	return (fact);
 }
