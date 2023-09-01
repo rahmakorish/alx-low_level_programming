417f263cea73c0b3dcc48a27186fbb2ceb988271
rahmakorish <rahmakorish@gmail.com>
2023-09-01 08:54:03 -0700

    adds

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *  * _strspn- gets the length of a prefix substring
+ *   * @s:string to search
+ *    * @accept: required prefix
+ *     * Return: number of bytes in initial segment
+ *      **/
+unsigned int _strspn(char *s, char *accept)
+{
+		int i = 0, x = 0;
+
+
+			while (*s != '\0')	/**increment pointer**/
+					{
+								while (*accept != '\0')
+											{
+															if (s[i] == accept[x])
+																			return (i + 1);
+																		else
+																						i++;
+																				}
+										x++;
+											}
+				return (i);
+}
+
