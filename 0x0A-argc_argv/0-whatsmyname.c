88883e6bfa9fccc21ad64a83b933bd2e34695965
rahmakorish <rahmakorish@gmail.com>
2023-09-01 02:19:10 -0700

    mynameis

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 
 #include <stdio.h>
 /**
- * print_name-prints the program own name
+ * main-prints the program own name
  * @argv: vector contain command line arguments
  * @argc: number of command line arguments
  * Return: program name
  **/
 int main(int argc, char **argv)
 {
-	printf("%s", argv[argc-1]);
+	printf("%s", argv[argc - 1]);
 	putchar('\n');
 	return (0);
-}	
+}
