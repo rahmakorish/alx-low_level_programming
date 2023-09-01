2c03546343005e3db6976aaa7575fd747d55ecf4
rahmakorish <rahmakorish@gmail.com>
2023-09-01 08:55:51 -0700

    adds

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ *  * _strpbrk- searches string for any set of bytes
+ *   * @s:string searched
+ *    * @accept: string we search for
+ *     **/
+char *_strpbrk(char *s, char *accept)
+{
+		unsigned int i;
+			
+			for(i = 0; i < strlen(s); i++)
+					{
+								if(s[i] == accept[i])
+											{	
+															printf("%d\n",s[i]);
+																		break;
+																				}
+									}
+				printf("%s\n",accept);
+					return(0);
+}
+
