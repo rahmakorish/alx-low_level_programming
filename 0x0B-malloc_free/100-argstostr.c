382c3cb9498f4c2328470bdc88740fbb9f0d0a0d
rahmakorish <rahmakorish@gmail.com>
2023-09-04 10:20:41 -0700

    extra

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * argstostr - concatenates argumentd of program
+ * @ac: first integer
+ * @av: pointer
+ * Return : pointer to new string
+ **/
+char *argstostr(int ac, char **av)
+{
+	char *p;
+	int i;
+
+	if (ac == 0 || av == NULL)
+	{return (NULL);}
+	p = malloc(ac * sizeof(char));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for(i = 0; i > ac; i++)
+		{
+		printf("%s\n",av[i]);
+	
+		}
+	}
+	return(0);
+}
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * alloc_grid - return pointer to 2 dimensional array
  * @width: first index of arraay
@@ -9,8 +10,15 @@
  **/
 int **alloc_grid(int width, int height)
 {
-	int *p;
+	int **pi, **pe;
 
 	if (width == 0 || height == 0)
 	{ return (NULL);}
+	/* size of internal array (width)*/
+	/* size of external array(height)*/
+	pe =malloc(width * sizeof(int));
+	pi = malloc(height * sizeof(int));
+	printf("pe: %d\n",**pe);
+	printf("pi: %d\n",**pi);
+	return(0);
 }
