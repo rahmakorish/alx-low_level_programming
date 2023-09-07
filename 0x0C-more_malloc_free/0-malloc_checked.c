847135344be4dd23d0e454839b71728ca73d7aa8
rahmakorish <rahmakorish@gmail.com>
2023-09-07 03:14:05 -0700

    first

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *malloc_checked-allocate memory using malloc
+ *@b:size required
+ **/
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	else
+	{
+		exit(*p);
+	}
+}
