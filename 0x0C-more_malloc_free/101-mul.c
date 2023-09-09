ccf84a74db63b4dcea9c67bb8d54589f9d443cab
rahmakorish <rahmakorish@gmail.com>
2023-09-09 16:12:49 -0700

    num

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,32 +1,30 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+/**check if nums are digit**/
+int check_int(char num)
+{
+	if (num > 48 && num < 58)
+	{
+	printf("%d\n",num);
+	return(num);}
+}
 /**
  * mul-multplies 2 nums
  * @num1:first number
  * @num2:second number
  * Return:pointer to new number
  **/
-void check_int(int num);
+int check_int(char num);
 void *mul(int num1, int num2)
 {
-	int x;
+/*	int x;
 	int *p1, *p2;
-(void) x,p1,p2;
+        (void)x,p1,p2;*/
 	if(check_int(num1) && check_int(num2))
 	{
 		printf("valid digit");
 	}
+	return(0);
 }
-/**check if nums are digit**/
-void check_int(int num)
-{
-	int i;
 
-	for (i = 0; i < 2; i++)
-	{
-		if (num[i] > 48 && num[i] < 58)
-		return (1);
-	}
-	return (0);
-}
