baa63fb3170b63977de5f0b915eac0e454380009
rahmakorish <rahmakorish@gmail.com>
2023-09-09 14:27:34 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,15 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  *_calloc-allocate memory for array
  *@nmemb:number of array elements
  *@size:size of element type
  *Return:pointer to new array
  **/
+char *setarray(char *ptr, char c, unsigned int x);
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *p;
-	unsigned int i;
+	char *p;
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -22,12 +23,23 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	}
 	else
 	{
-		for (i = 0; i < (nmemb * sizeof(int)); i++)
-		{
-			p[i] = 0;
-			return (p);
-		}
+		setarray(p, 0, nmemb * sizeof(int));
 	}
-	free(p);
+	return (p);
 	exit(0);
+}
+/**
+ *setarray-initalize array to zero
+ *@ptr:pointer
+ *@c:character to fill
+ *@x:sizeof array
+ *Return: pointer to zero array
+ **/
+char *setarray(char *ptr, char c, unsigned int x)
+{
+	while(x--)
+	{
+		*ptr++ = c;
+	}
+	return (ptr);
 }
