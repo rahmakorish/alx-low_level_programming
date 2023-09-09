7a6990b231abbae704b411bce13844c35f29e75f
rahmakorish <rahmakorish@gmail.com>
2023-09-09 13:25:31 -0700

    first

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,11 +15,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 	{
-		s1 = "";
+		s1 = " ";
 	}
 	if (s2 ==  NULL)
 	{
-		s2 = "";
+		s2 = " ";
 	}
 	len1 = strlen(s1);
 	p = malloc((len1 + n + 1) * sizeof(char));
