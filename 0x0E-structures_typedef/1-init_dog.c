09b8b4208db12a55e8e3be1b4745cd2eb77a1711
rahmakorish <rahmakorish@gmail.com>
2023-09-11 03:02:53 -0700

    dogoo

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * init_dog-initialize variable of type struct dog
+ * @d:dog variable type
+ * @name:dog's name
+ * @age:number age
+ * @owner:dog's owner
+ * Return:init_dog struct type
+ **/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
