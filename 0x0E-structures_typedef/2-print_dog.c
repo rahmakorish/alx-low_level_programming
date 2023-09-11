3dc1c3c232027342d54b2acdfebebacc00198c02
rahmakorish <rahmakorish@gmail.com>
2023-09-11 10:51:14 -0700

    fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,9 +7,13 @@
  **/
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d->name == NULL)
 	{
-		exit(98);
+		d->name = "(nil)";
+	}
+	if (d->owner == NULL)
+	{
+		d->owner = "(nil)";
 	}
 	else
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
