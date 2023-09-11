e384e9ead2d1c89c3d7b6f56870a3549d08c8ef2
rahmakorish <rahmakorish@gmail.com>
2023-09-11 10:46:02 -0700

    fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,10 +10,10 @@
  **/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t dogt;
+	struct dog_t *dogt;
 
-	dogt.name = name;
-	dogt.age = age;
-	dogt.owner = owner;
+	dogt->name = name;
+	dogt->age = age;
+	dogt->owner = owner;
 	return(0);
 }
