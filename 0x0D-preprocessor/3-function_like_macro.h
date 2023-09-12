e0fdd51d6861172690d032f1d03eb599f550f34e
rahmakorish <rahmakorish@gmail.com>
2023-09-12 13:06:11 -0700

    fix

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,11 @@
+#ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
+
 #define ABS(x) 
-#if x < 0
-#define x -x;
-#elif x > o
-#define x x;
+#if (x) < (0)
+#define (x) (-x)
+
+#elif (x) > o
+#define (x) (x)
+#endif
 #endif
