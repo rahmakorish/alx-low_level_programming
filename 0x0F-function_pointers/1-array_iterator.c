25595503bafe1860968451d8c24d6db99bb16f52
rahmakorish <rahmakorish@gmail.com>
2023-09-13 09:56:06 -0700

    array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * array_iterator-prints array
+ * @array:array to be printed
+ * @action:pointer to function
+ * @size:size of array
+ * Return:0success
+ **/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	while (i < size)
+	{
+	action(array[i]);
+	i++;
+	}
+}
