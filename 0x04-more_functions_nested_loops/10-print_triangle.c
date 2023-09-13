5d4c4928c40a5063ea3038fe1ce75f5a526dc533
rahmakorish <rahmakorish@gmail.com>
2023-09-13 13:53:51 -0700

    delete

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,26 +6,40 @@
  **/
 void print_triangle(int size)
 {
-	int symbol = 0;
-	int space = size;
+/**	int symbol;9 space 1 symbol  space = size - symbol 
+		     **8 space 2symbol**/
+	int space = size, symbol = 1;
+/**
+	for (space = size; space > 0; space--)
+	{
+		
+		for(symbol = 1; symbol < size; symbol++)
+		{
+			_putchar(' ');
+			_putchar('#');
+		};
+		_putchar('\n');
+	}
 
+}**/
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		while (space >= 0)
+		while (space >= size - symbol)
 		{
 			_putchar(' ');
-			while ( symbol < size)
+			while (symbol < size)
 			{
 				_putchar('#');
-				symbol++;
-				/*_putchar('\n');*/
-			};
-			putchar('\n');
+			}
+			symbol++;
 			space--;
+			_putchar('\n');
+			
 		}
-	};
+
+	}
 }
