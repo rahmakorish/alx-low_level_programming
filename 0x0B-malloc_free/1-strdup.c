eefce5867326425925f254743492b5c67de70462
rahmakorish <rahmakorish@gnail.com>
2023-10-03 09:37:07 -0700

    l

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_strdup-copy string and return pointer
+ *@str:string to be copyed
+ *Return:pointer to copied string
+ **/
+char *_strdup(char *str)
+{
+	int i, len = 0;
+	char *p;
+
+	if (str == NULL)
+	return (NULL);
+	else
+	{
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	p = malloc(sizeof(char) * len);
+	if (p == NULL)
+	return (NULL);
+	else
+	{
+		for (i = 0; i <= len; i++)
+			p[i] = str[i];
+	}
+	return (p);
+	}
+}
