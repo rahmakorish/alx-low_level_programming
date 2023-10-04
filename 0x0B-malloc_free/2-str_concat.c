d501cd43e284477a8e33cc53faa111b2df24dc55
rahmakorish <rahmakorish@gnail.com>
2023-10-04 11:29:40 -0700

    cat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,17 +13,14 @@ char *str_concat(char *s1, char *s2)
 	int len1 = 0, len2 = 0, i = 0, x = 0;
 	char *cat, *tmp1 = s1, *tmp2 = s2;
 
-	if(s1 == NULL)
-	{
-		s1 = " ";
+	if (s1 == NULL)
+	{s1 = "";
 	}
 	if (s2 == NULL)
-	{
-		s2 = " ";
+	{s2 = "";
 	}
 	while (*s1 != '\0')
-	{
-		len1++;
+	{len1++;
 		s1++;
 	}
 	while (*s2 != '\0')
