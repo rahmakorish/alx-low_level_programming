b3af8fcfaf2286687437ce8331334c77cf9c17c1
rahmakorish <rahmakorish@gnail.com>
2023-10-10 04:21:38 -0700

    dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_dog-initialize variable of type struct dog
+ * @d:dog variable type
+ **/
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		exit(98);
+	}
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+}
+
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * new_dog-creates a new dog
+ * @name:string 
+ * @age:number
+ * @owner:name
+ * Return:new object
+ **/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog_t *dogt;
+
+	dogt->name = name;
+	dogt->age = age;
+	dogt->owner = owner;
+	return(0);
+}
+
