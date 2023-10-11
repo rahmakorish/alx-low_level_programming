f1f93d6262801ef4ac6816ce7485da5ab65a1d49
rahmakorish <rahmakorish@gnail.com>
2023-10-11 06:24:18 -0700

    initial

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * print_name-prints a name
+ * @name:name to be printed
+ * @f:pointer to functon
+ * Return:0success
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+
+	if (name == NULL)
+	{
+		exit(98);
+	}
+	f(name);
+}
