0d0a5821a434a9e625a9ac3a2edd8f21156aae0f
rahmakorish <rahmakorish@gnail.com>
2023-10-12 10:44:34 -0700

    fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,12 +2,26 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <ctype.h>
+#include <stdlib.h>
 /**
  * print_strings-prints strings
  * @separator:string to be printed between strings
  * @n:inputted nums
  * Return:0success
  **/
+char *convert_int_to_string(va_list arg) {
+  int value = va_arg(arg, int);
+
+  char *buffer = malloc(10);
+  if (buffer == NULL) {
+    return NULL;
+  }
+
+  sprintf(buffer, "%d", value);
+
+  return buffer;
+}
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i = n;
@@ -16,14 +30,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(arg, n);
 	while (i != 0)
 	{
-		if (separator == NULL)
-		{printf("%d ", va_arg(arg, int));
+		if ((va_arg(arg, int)))
+		{
+			convert_int_to_string(arg);
+			printf("%i%s",va_arg(arg, int),separator);
 		i--;
 		}
-		if (isalpha(va_arg(arg, int)))
-		{printf("%d%s",va_arg(arg, int),separator);
-		i--;
-		}
+		(void)separator;
 	}
 	printf("\n");
 	va_end(arg);
