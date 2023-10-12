127ea96d79bd4ef6c9d267f5456ad6a5a39fbd3f
rahmakorish <rahmakorish@gnail.com>
2023-10-12 09:21:42 -0700

    2nd

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers-prints numbers
+ * @separator:string to separate nums
+ * @n:nums
+ * Return:0success
+ **/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list arg;
+	unsigned int i = n;
+
+	va_start(arg, n);
+	while (i != 0)
+	{
+		printf("%i%s", va_arg(arg, int), separator);
+		i--;
+	}
+	printf("\n");
+	va_end(arg);
+}
