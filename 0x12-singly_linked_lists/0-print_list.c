75f947df990577332e71febc3e4e92731850a681
rahma korish <rahmakorish@gmail.com>
2023-10-19 05:06:02 -0700

    start

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * print_list-prints all elements of a list
+ * @h:inputted list
+ * Return:number of nodes
+ **/
+size_t print_list(const list_t *h)
+{
+	if (h == NULL)
+	{
+		printf("[0] (nil)");
+	}
+	return(0);
+}
