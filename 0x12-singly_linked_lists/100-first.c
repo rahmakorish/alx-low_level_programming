2f75a0ee7cd441f2a1d45a38e5a975f57a2ea4ea
rahma korish <rahmakorish@gmail.com>
2023-10-19 14:42:45 -0700

    gg

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_first-prints You're beat! and yet, you must allow,\nI bore my house upon my back!\n before the main function is executed.
+ * Return:0success
+ **/
+void print_first()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -26,18 +26,15 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 	temp->str = copy;
 	temp->next = NULL;
+	temp->len = strlen(temp->str);
 	if (temp->str == NULL)
 	{
 		free(temp);
 		return (NULL);
-	}
-	}
-
+	}}
 	/*iterate over copy original adresses*/
-	while (ptr->next != NULL)
-	{
-		ptr = ptr->next;
-	}
+	while (ptr != NULL)
+	{ptr = ptr->next;}
 	ptr->next = temp;
 	*head = ptr;
 	return(*head);
