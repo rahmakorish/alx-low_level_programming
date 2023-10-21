15122e7d5a419bd4e6200661814fee35ff14e010
rahma korish <rahmakorish@gmail.com>
2023-10-21 07:35:50 -0700

    update

--- 0x10-variadic_functions/3-main.c
+++ 0x10-variadic_functions/3-main.c
@@ -1,12 +1,13 @@
+#include <stdlib.h>
 #include "variadic_functions.h"
-#include <stddef.h>
+
 /**
- * main - check the code
+ * main - check the code .
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    print_all("qwertyuopadghjklzxvbnm","hi");
-    return (0);
+	print_all("fcsi", 3.14435, 'H', NULL, 402);
+	return (0);
 }
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,20 +5,63 @@
 #include <stdlib.h>
 #include <string.h>
 /**
+ * _strtlen-measure length of constant
+ * @con:given const
+ * Return:length of input
+ **/
+int _strlen(const char *con)
+{
+	int len = 0;
+
+	while (*con)
+	{len += 1;
+	con++;
+	}
+	return (len);
+}
+/**
  * print_all-prints strings
  * @format:inputted nums
  * Return:0success
  **/
 void print_all(const char * const format, ...)
-{const char *i = format;
+{      
 	va_list arg;
 	char *input;
+	const char *str = format;
+	int i = 0, num;
+	float num1;
 
 	va_start(arg, format);
-	while (*i != '\0')
-	{
-		printf("%s", (input = va_arg(arg, char*)));
-	break;
-	}
+	while (format && format[i])
+	{while (i < (_strlen(format)))
+		{switch (str[i])
+			{case 's':
+				input = va_arg(arg, char*);
+				printf ("%s", input);
+				break;
+				case 'i':
+				num = va_arg(arg, int);
+				printf("%i", num);
+				break;
+				case'f':
+				num1 = va_arg(arg, double);
+				printf("%f", num1);
+				break;
+				case 'c':
+				printf ("%c", va_arg(arg, int));
+				break;
+				break;
+				default:
+				break;
+			}
+			 	i++;
+				if (i == _strlen(format))
+				{break;
+				}
+				printf(", ");
+			}
+		}
+	
 	printf("\n");
 }
