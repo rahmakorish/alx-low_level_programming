2c27f74d7e463f5e2cec42631eb55e2d9884ddf1
rahma korish <rahmakorish@gmail.com>
2023-10-23 03:05:36 -0700

    g

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * add_nodeint_end-add node to end of list
+ * @head:list
+ * @n:data 
+ * Return:adress of new element
+ **/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *temp;
+	listint_t *ptr = *head;
+
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+	{return (NULL);
+	}
+	if (n)
+	{
+		temp->n = n;
+		if (!temp->n)
+		{
+			free(temp);
+			return(NULL);
+		}
+	}
+	while (ptr)
+	{
+		ptr = ptr->next;
+	}
+	ptr->next = temp;
+	*head = temp;
+	return (*head);
+}
