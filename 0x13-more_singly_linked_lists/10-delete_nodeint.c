dd50fca2eeea2660523779011a9eb990bf3ee564
rahma korish <rahmakorish@gmail.com>
2023-10-23 14:15:20 -0700

    delete

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index-deletes node of certain index
+ * @head:list
+ * @index:index needed
+ * Return:1success
+ **/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *current = *head;
+	listint_t *previous = *head;
+
+	if (head)
+	{
+		return (-1);
+	}
+
+	while (index != 1)
+	{
+		previous = current;
+		current = current->next;
+		index--;
+	}
+	previous->next = current->next;
+	free(current);
+	current = NULL;
+	return (1);
+}
