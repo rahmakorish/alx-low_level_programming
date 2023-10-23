5636e2c4fa98fd38664fa51f28fb5c252e59e0bd
rahma korish <rahmakorish@gmail.com>
2023-10-23 11:27:59 -0700

    ip

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ *insert_nodeint_at_index-inserts a node at position
+ *@head:start list
+ *@idx:index of list 
+ *@n:data of node
+ *Return:adress of node
+ **/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp;
+	listint_t *ptr = *head;
+	unsigned int pos = idx - 1;
+
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	temp->n = n;
+	if (!temp->n)
+	{
+		free(temp);
+		return (NULL);
+	}
+	while (idx != pos)
+	{
+	ptr = ptr->next;
+	idx--;
+	}
+	temp->next = ptr->next;
+	ptr->next= temp;
+
+	return (temp);
+}
