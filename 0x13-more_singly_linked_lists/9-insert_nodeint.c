e2c1f560b6428e24e1cc40de88e9ff55939579a4
rahma korish <rahmakorish@gmail.com>
2023-10-24 05:44:56 -0700

    h

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp;
 	listint_t *ptr = *head;
 	listint_t *cptr = *head;
-	unsigned int count = -1;
+	unsigned int count = 0;
 
 	while (cptr)
 	{
