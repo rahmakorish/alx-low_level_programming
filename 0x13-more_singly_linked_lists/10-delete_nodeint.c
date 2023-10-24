1fa2908e908f979c5a28253fa486b4b676a0af2e
rahma korish <rahmakorish@gmail.com>
2023-10-24 11:12:28 -0700

    g

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,12 +10,19 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *current;/* = *head;*/
 	listint_t *previous; /*= *head;I*/
 
-	if (head)
+	if (!head || !(*head))
 	{
 		return (-1);
 	}
 	current = *head;
 	previous = *head;
+	if (!index)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+		return (1);
+	}
 	while (index != 1)
 	{
 		previous = current;
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,7 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = temp;
 		return (temp);
 	}
-	else if (idx < count)
+	else
 	{
 		while (idx != 1)
 		{
