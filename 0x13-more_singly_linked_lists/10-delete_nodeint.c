49b6fdd4cf815ff08363f60f4238362f627b9bf6
rahma korish <rahmakorish@gmail.com>
2023-10-24 05:40:32 -0700

    g

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *current;/* = *head;*/
 	listint_t *previous; /*= *head;I*/
 
-	if (head == NULL)
+	if (head)
 	{
 		return (-1);
 	}
