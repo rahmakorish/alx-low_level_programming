b93300a3cfc75ee37cd202408279f8e7e12b920a
rahma korish <rahmakorish@gmail.com>
2023-10-24 12:40:29 -0700

    f

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -6,5 +6,20 @@
  **/
 listint_t *reverse_listint(listint_t **head)
 {
+	listint_t *temp, *temp2;
 
+	if(!head || !(*head))
+	return (NULL);
+	temp2 = NULL;
+	temp = NULL;
+
+	while (head != NULL)
+	{
+		temp2 = (*head)->next;
+		(*head)->next = temp;
+		temp = *head;
+		*head = temp2;
+	}
+	*head = temp;
+	return (*head);
 }
