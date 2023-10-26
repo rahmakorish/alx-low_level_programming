a40da5aa15c1ef67a8762fb803d412f7156707dd
rahma korish <rahmakorish@gmail.com>
2023-10-26 09:39:26 -0700

    1st

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * binary_to_uint-converts binary to unsigned int
+ * @b:string binary
+ * Return:converted number
+ **/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0, weight = 1;
+	unsigned int rem, numb;
+
+	if (!b)
+	return (0);
+	else
+	{
+		numb = atoi(b);
+		while (numb)
+		{
+			rem = numb % 10;
+			decimal = decimal + rem * weight;
+			weight = weight * 2;
+			numb = numb / 10;
+		}
+	}
+		return (rem);
+	
+}
