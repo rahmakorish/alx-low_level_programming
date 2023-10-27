5277b715cf911772cb64d040374996c1e4a641f1
rahma korish <rahmakorish@gmail.com>
2023-10-27 13:41:13 -0700

    adv

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,9 +13,14 @@ unsigned int binary_to_uint(const char *b)
 	return (0);
 	else
 	{
+		if (*b != '0' && *b != '1')
+		{return (0);
+		}
 		numb = atoi(b);
 		while (numb)
 		{
+			/*if (numb != '0' && numb != '1')
+			{ return (0);}*/
 			rem = numb % 10;
 			decimal = decimal + rem * weight;
 			weight = weight * 2;
