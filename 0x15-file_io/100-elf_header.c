60c08432a491bb42e4d04fabb31a06fbc2f453d2
rahma korish <rahmakorish@gmail.com>
2023-10-30 14:34:26 -0700

    f

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -35,6 +35,89 @@ void print_class(Elf64_Ehdr h)
 	printf("\n");
 }
 /**
+ * print_data-prints elf magic
+ * @h: elf header
+ **/
+void print_data(Elf64_Ehdr h)
+{
+	printf("  Data:       ");
+	switch (h.e_ident[EI_CLASS])
+	{
+		case ELFDATA2MSB:
+			printf("2's complement, big endian");
+			break;
+		case ELFDATA2LSB:
+			printf("2's complement, little endian");
+			break;
+		case ELFDATANONE:
+			printf("none");
+			break;
+	}
+	printf("\n");
+}
+/**
+ * print_version-prints elf magic
+ * @h: elf header
+ **/
+void print_version(Elf64_Ehdr h)
+{
+	printf(" Version:      %d", h.e_ident[EI_VERSION]);
+	switch (h.e_ident[EI_VERSION])
+        {
+                case EV_CURRENT:
+                        printf(" (Ccurrent) ");
+                        break;
+                case EV_NONE:
+                        printf("%s", "");
+                        break;
+		}
+	printf("\n");
+}
+/**
+ * print_abiversion-prints elf magic
+ * @h: elf header
+ **/
+void print_abiversion(Elf64_Ehdr h)
+{
+	printf("  ABI Version:       %d\n", h.e_ident[EI_ABIVERSION]);
+}
+/**
+ * print_type-prints elf magic
+ * @h: elf header
+ **/
+void print_type(Elf64_Ehdr h)
+{
+	char *p = (char *)&h.e_type;
+	int i = 0;
+
+        printf("  Type:       ");
+       if (h.e_ident[EI_DATA] == ELFDATA2MSB)
+	       i = 1;
+       switch (p[i])
+       {
+	       case ET_NONE:
+		       printf("NONE (NONE)");
+		       break;
+		case ET_REL:
+		       printf("REL (Relocatable file)");
+		       break;
+		case ET_EXEC:
+		       printf("EXEC (Executable file)");
+		       break;
+		case ET_DYN:
+		       printf("DYN (Shared object file)");
+		       break;
+		case ET_CORE:
+		       printf("CORE (Core file)");
+		       break;
+		default:
+		       printf("<unknown>: %x", p[i]);
+			break;
+       }
+       printf("\n");
+}
+
+/**
  * main-copies the content of a file to another
  * @argc:argument count
  * @argv:argument vector
@@ -64,12 +147,12 @@ int main(int argc, char *argv[])
 	}
 	print_magic(h);
 	print_class(h);
-/*	print_data(h);
+	print_data(h);
 	print_version(h);
-	print_osabi(h);
-	print_obiversion(h);
+	/*print_osabi(h);*/
+	print_abiversion(h);
 	print_type(h);
-	print_entry(h);*/
+	/*print_entry(h);*/
 	if (close(fd))
 	{
 	dprintf(STDOUT_FILENO, "Erroe closing file: %d\n", fd);
