ff0222a07389736ce5e9dd542df6452ee2aaabb8
rahma korish <rahmakorish@gmail.com>
2023-10-30 13:16:55 -0700

    f

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,10 +7,10 @@
 int _strlen(char *str)
 {
 	int count = 0;
-	
+
 	if (str == NULL)
 		return (0);
-	while(*str++)
+	while (*str++)
 	count++;
 	return (count);
 }
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 	newfile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (newfile == -1)
 		return (-1);
-	if (len)	
+	if (len)
 	byte = write(newfile, text_content, len);
 	close(newfile);
 	return (byte == len ? 1 : -1);
