f75067399f9299be178dadfb7c554aa38786eb17
rahma korish <rahmakorish@gmail.com>
2023-10-30 06:19:23 -0700

    2nd

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,17 @@ int create_file(const char *filename, char *text_content)
 	{perror("open");
 		return (-1);
 	}
+	if (write(newfile, text_content, sizeof(text_content) - 1) == -1)
+	{
+		perror("write");
+		return (-1);
+	}
 	write(newfile, text_content, sizeof(text_content) - 1);
+	if (close(newfile) == -1)
+	{
+		perror("Close");
+		return (-1);
+	}
 	close(newfile);
 	return (1);
 }
