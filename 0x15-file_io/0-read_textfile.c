258b91da5f2e834844114641c75e9cc993c06c7c
rahma korish <rahmakorish@gmail.com>
2023-10-30 08:38:22 -0700

    d

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,10 +6,8 @@
  * Return:number of letters
  **/
 ssize_t read_textfile(const char *filename, size_t letters)
-{
-	char *string= " ";
+{char *string = "hello";
 	int file;
-       size_t i;
 
 	if (filename == NULL)
 		return (0);
@@ -19,11 +17,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		perror("open");
 		return (0);
 	}
-	dprintf(file,"opened\n");
-	/*for (i = 0; i < letters; i++)
-	{string[i] = filename[i];
-	}*/
-	dprintf(file,"string done");
 	if (write(file, string, letters) == -1)
 	{
 		perror("write");
@@ -35,8 +28,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		perror("close");
 		return (0);
 	}
-	write(file,string,letters);
-	(void)i;
-	dprintf(file, "%s", string);
+	dprintf(STDOUT_FILENO, "%s", string);
 	return (letters);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,16 +11,23 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_RDWR | O_APPEND);
+	file = open(filename, O_RDWR | O_APPEND, 0661);
 	if (!file)
 	{perror("open");
 		return (-1);
 	}
-	if (sizeof(text_content) - 1)
+	if (text_content == NULL)
+	{
+		close(file);
+		return (1);
+		exit(0);
+	}
+	else if (text_content)
 	{
 	write(file, text_content, sizeof(text_content) - 1);
 	write(file, "\n", 1);
 	close(file);
 	return (1);
 	}
+	return (1);
 }
