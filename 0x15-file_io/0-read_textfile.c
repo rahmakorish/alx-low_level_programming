524908288fce1cb755bb1085946caac774103730
rahma korish <rahmakorish@gmail.com>
2023-10-30 05:29:24 -0700

    start

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * read_textfile-functon that read text file and prints it
+ * @filename:file name
+ * @letters:number of letters read and printed
+ * Return:number of letters
+ **/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *string = "hello";
+	int file;
+
+	if (filename == NULL)
+		return (0);
+	file = open(filename, O_RDWR, 0600);
+	if (file == -1)
+	{
+		perror("open");
+		return (0);
+	}
+	if (write(file, string, letters) == -1)
+	{
+		perror("write");
+		return (0);
+	}
+	dprintf(file, "%s", string);
+	if (close(file) == -1)
+	{
+		perror("close");
+		return (0);
+	}
+	dprintf(file, "%s", string);
+	return (letters);
+}
