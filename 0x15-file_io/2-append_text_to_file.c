71dac9085134431390059f19b121648d93e297c4
rahma korish <rahmakorish@gmail.com>
2023-11-03 16:28:51 -0700

    fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,26 +23,19 @@ int _strlen(char *str)
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int file;
+	ssize_t byte = 0, len = _strlen(text_content);
 
 	if (filename == NULL)
 		return (-1);
 	file = open(filename, O_RDWR | O_APPEND, 0661);
-	if (!file)
-	{perror("open");
+	if (file == -1)
+	{
 		return (-1);
 	}
-	if (text_content == NULL)
+	if (len)
 	{
-		close(file);
-		return (1);
-		exit(0);
+		byte = write(file, text_content, len);
 	}
-	else if (text_content)
-	{
-	write(file, text_content, _strlen(text_content));
-	write(file, "\n", 1);
 	close(file);
-	return (1);
-	}
 	return (1);
 }
