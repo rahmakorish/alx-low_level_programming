695f22a9afa28eecc33727a2c921088d0be561bc
rahma korish <rahmakorish@gmail.com>
2023-12-07 09:06:43 -0800

    0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,2 +1,18 @@
 #include "lists.h"
+/**
+ *print_dlistint-prints the elements of a list
+ *@h:linked list
+ *Return:number of nodes
+ **/
 size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count += 1;
+		printf("%i\n", h->n);
+		h = h->next;
+	}
+return (count);
+}
