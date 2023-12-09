c9cc203717058fd3377e8e6c73fd7925aaa56a57
rahma korish <rahmakorish@gmail.com>
2023-12-09 12:38:31 -0800

    add

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,15 +7,24 @@
  **/
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	if (!head)
-		return (NULL);
-	while (head != NULL)
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	if (!head || !new)
 	{
-		if (head->next == NULL)
+		if (new)
+			free(new);
+		return (NULL);
+	}
+	new->next = NULL;
+	new->n = n;
+	while (*head != NULL)
+	{
+		if ((*head)->next == NULL)
 		{	
-			printf("last %i\n",n);
-			head = head->next;
+			printf("last %i\n", n);
+			(*head)->next = new;
 		}
+		else
+			printf("working");
 
 	}
 	return (*head);
