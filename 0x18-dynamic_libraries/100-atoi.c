444ccdd3549d3e697b74f9be7eee57304e3c5c56
rahma korish <rahmakorish@gmail.com>
2023-12-11 01:56:55 -0800

    files

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *  *_islower-check user input
+ *   *Description:this function checks user input
+ *    *@c:input
+ *     *Return:0iflowerchar
+ *      *1ifupper
+ *       **/
+int _atoi(char *s)
+{}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *  * _abs-compute the absloute value of integer
+ *   * @x:user input
+ *    * Return:absloute value of input
+ *     **/
+int _abs(int x)
+{
+		int z = x % 10;
+
+			if(x > 0)
+					{	
+								return (z);
+									}
+				else if (x < 0)
+						{
+									z= z * -1;
+											return (z);
+												};
+}
