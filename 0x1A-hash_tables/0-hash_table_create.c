c08006731582607fd1affa0c67b5824ccf24e2fa
rahma korish <rahmakorish@gmail.com>
2023-12-21 12:28:44 -0800

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,6 +6,7 @@
  **/
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t *new = malloc(sizeof(hash_table_t));
 
 	if (new == NULL)
@@ -20,6 +21,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 		{
 			free(new);
 			return (NULL);
+		}
+		else
+		{
+			for (i = 0; i < size; i++)
+				new->array[i] = NULL;
 		}
 	}
 	return (new);
