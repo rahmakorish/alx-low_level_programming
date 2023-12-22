9a8bc134e24d2578f2ad5186e14164eef3223d08
rahma korish <rahmakorish@gmail.com>
2023-12-22 04:17:01 -0800

    5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,16 +7,21 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int index;
 
+	if (ht == NULL)
+		return;
 	printf("{");
 	for (index = 0; index < ht->size; index++)
 	{
 		if (ht->array[index] != NULL)
 		{
 			if (index != ht->size)
-			printf("'%s': '%s',",ht->array[index]->key,ht->array[index]->value);
-			else if (index == ht->size - 1)
-			printf("'%s': '%s'\n}",ht->array[index]->key,ht->array[index]->value);
+			printf("'%s': '%s', ", ht->array[index]->key, ht->array[index]->value);
+			else 
+			printf("'%s': '%s'\n}", ht->array[index]->key, ht->array[index]->value);
 		}
+		else
+			continue;
 		
 	}
+	printf("}\n");
 }
