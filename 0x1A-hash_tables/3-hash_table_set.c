4269b2a823eefc90286a39179dc0b05363693825
rahma korish <rahmakorish@gmail.com>
2023-12-22 07:13:20 -0800

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,14 +9,16 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	hash_node_t *new_node = malloc(sizeof(hash_node_t *));
 
+	if (new_node == NULL)
+		return (0);
 	if (key[0] == '\0')
 		return (0);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	index = key_index((const unsigned char *)key, ht->size);
-	if (index <= ht->size)
+	if (index < ht->size)
 	{
 		if (ht->array[index] == NULL)
 		{
