ef342f87ece548642e2bfd4d8beb50e74f26f10c
rahma korish <rahmakorish@gmail.com>
2023-12-22 07:23:57 -0800

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (key[0] == '\0')
 		return (0);
 	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+		return (0);
 	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	if (index < ht->size)
 	{
