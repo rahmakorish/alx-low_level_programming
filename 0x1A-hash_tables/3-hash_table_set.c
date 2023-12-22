0298b651ac368eb87b6c15922494a41c7dc99686
rahma korish <rahmakorish@gmail.com>
2023-12-22 07:16:59 -0800

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,8 +26,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		else
 		{
+			if (ht->array[0] != NULL)
+				ht->array[0]->next = new_node;
+			else
+			{
 			index = 0;
 			(ht->array[index]) = new_node;
+			}
 		}
 	}
 	return (1);
