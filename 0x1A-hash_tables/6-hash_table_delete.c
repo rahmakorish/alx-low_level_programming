755fe2bc89c75963235045f2976efaa329f05e28
rahma korish <rahmakorish@gmail.com>
2023-12-22 09:10:32 -0800

    6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -5,20 +5,26 @@
  **/
 void hash_table_delete(hash_table_t *ht)
 {
-	long unsigned int i;
-	/*hash_node_t *tmp;*/
+	unsigned long int i;
+	hash_node_t *tmp, *next;
 
 	if (ht == NULL)
-	free (ht);
+	free(ht);
 	else if (ht != NULL)
 	{
 		if (ht->array != NULL)
 		{
 			for (i = 0; i < ht->size; i++)
 			{
-				/*tmp = ht->array[i];
-				ht->array[i] = NULL;*/
-				free(ht->array[i]);
+				tmp = ht->array[i];
+				while (tmp != NULL)
+				{
+					next = tmp->next;
+					free(tmp->value);
+					free(tmp->key);
+					free(tmp);
+					tmp = next;
+				}
 			}
 		}
 	}
