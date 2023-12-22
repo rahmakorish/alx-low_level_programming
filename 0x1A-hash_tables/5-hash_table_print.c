607b5af58f44f86aaf9bed9bce4bbc647c4e106c
rahma korish <rahmakorish@gmail.com>
2023-12-22 11:17:36 -0800

    5 final

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -5,9 +5,8 @@
  **/
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int index;
+	unsigned long int index, first = 1;
 	hash_node_t *node;
-	int first = 1;
 
 	if (ht == NULL)
 		return;
@@ -23,6 +22,7 @@ void hash_table_print(const hash_table_t *ht)
 					printf(", ");
 				printf("'%s': '%s'", ht->array[index]->key, ht->array[index]->value);
 				first = 0;
+				node = node->next;
 			}
 		}
 		else
