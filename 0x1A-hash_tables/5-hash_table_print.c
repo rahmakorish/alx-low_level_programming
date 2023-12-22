db5a66c9010ba3f68dca551c6c4b83045c5eb74c
rahma korish <rahmakorish@gmail.com>
2023-12-22 03:54:20 -0800

    5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,1 +1,22 @@
 #include "hash_tables.h"
+/**
+ *hash_table_print- a function that prints a hash table.
+ *@ht:the hash table
+ **/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index;
+
+	printf("{");
+	for (index = 0; index < ht->size; index++)
+	{
+		if (ht->array[index] != NULL)
+		{
+			if (index != ht->size)
+			printf("'%s': '%s',",ht->array[index]->key,ht->array[index]->value);
+			else if (index == ht->size - 1)
+			printf("'%s': '%s'\n}",ht->array[index]->key,ht->array[index]->value);
+		}
+		
+	}
+}
