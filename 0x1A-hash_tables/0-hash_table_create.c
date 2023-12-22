1b4d8f05abfea68c2ab1e8409a078c759ce867ea
rahma korish <rahmakorish@gmail.com>
2023-12-22 04:04:27 -0800

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,6 +6,7 @@
  **/
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t *new = malloc(sizeof(hash_table_t));
 
 	if (new == NULL)
@@ -15,12 +16,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	else
 	{
 		new->size = size;
-		new->array = malloc(sizeof(hash_node_t) * size);
+		new->array = malloc(sizeof(hash_node_t *) * size);
 		if (new->array == NULL)
 		{
 			free(new);
 			return (NULL);
 		}
+		for (i = 0; i < new->size; i++)
+			new->array[i] = NULL;
 	}
 	return (new);
 }
