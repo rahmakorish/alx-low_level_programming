2fddc91ba8fe23effe2fe27a89f63119efbb186b
rahma korish <rahmakorish@gmail.com>
2024-01-28 05:19:46 -0800

    advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  *main - this entry point for the program
+ *   *Return: 0 if code executed and a positive number otherwise
+ *    **/
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
